97dd0f5a14be4f41ecbf58c195e2f4b9166c70ea
alejandroharb <alejandroharb@gmail.com>
2017-02-02 12:46:57 -0600

    finished seeds for bamazon_db

---
+++ seeds.sql
@@ -0,0 +1,20 @@
+INSERT INTO products (product_name, department_name, price, stock_quantity, product_sales)
+VALUES("Lenovo Yoga Laptop", "electronics", 1100.99, 10),
+("iPad", "electronics", 400, 8),
+("Mountain Bike", "outdoors", 400, 6),
+("Smart T.V.", "electronics", 1200, 4),
+("Levi Jeans", "Mens Clothing", 34..99, 20),
+("Starbucks Coffee Beans", "food and beverage", 9.99, 26),
+("tent", "outdoors", 700.00, 6),
+("Apple Watch", "electronics", 500, 3),
+("Mens belt", "Mens Clothing", 14.99, 5),
+("Bananas", "food & beverage", 1.99, 40);
+
+INSERT INTO departments (department_name, over_head_costst)
+VALUES("electronics", 15000),
+("outdoors", 20000),
+("Mens Clothing", 1000),
+("food and beverage", 5000),
+("Womens Clothing", 3000),
+("Lawns", 2000),
+("Tools", 2000);
